54fe5e7d0fd1bcd2e334f1fc3841f62d1efa1e24
vignesh235 <m.vignesh@thrivusoft.in>
2022-06-30 18:53:11 +0530

    customize the project doc type

--- sgp/sgp/function_calling.py
+++ sgp/sgp/function_calling.py
@@ -6,7 +6,8 @@ from sgp.sgp.utils.buying.purchase_receipt import purchase_receipt
 from sgp.sgp.utils.buying.request_for_quotation import request_for_quotation
 from sgp.sgp.utils.buying.supplier_quotation import supplier_quotation
 from sgp.sgp.utils.selling.sales_invoice.sales_invoice_custom_fields import sales_invoice_customization
-
+from sgp.sgp.utils.projects.site_work.site_work import customize_field
+from sgp.sgp.utils.projects.site_work.site_work import site_doc_name 
 def function_calling():
     purchase_invoice()
     purchase_order()
@@ -15,5 +16,6 @@ def function_calling():
     supplier_quotation()
     item_customization()
     sales_invoice_customization()
-  
+    customize_field()
+    site_doc_name()
 
