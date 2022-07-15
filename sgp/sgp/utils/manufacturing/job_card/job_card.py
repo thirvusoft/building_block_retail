b65b73fde156c48a60bd2fecacddc43995c6f7b8
root <root@ip-172-26-3-215.ap-south-1.compute.internal>
2022-07-15 10:13:29 +0000

    live site changes

--- sgp/sgp/utils/manufacturing/job_card/job_card.py
+++ sgp/sgp/utils/manufacturing/job_card/job_card.py
@@ -27,5 +27,6 @@ def create_property_setter():
     make_property_setter(doctype, 'quality_inspection_template', 'hidden', '1', 'Check')
     make_property_setter(doctype, 'batch_no', 'hidden', '1', 'Check')
     make_property_setter(doctype, 'more_information', 'hidden', '1', 'Check')
+    make_property_setter(doctype, 'wip_warehouse', 'hidden', '1', 'Check')
 def before_submit(self, event):
     if(not self.se_created):frappe.throw("Please click <b>Finish</b> button to create stock entry and then submit this.")
