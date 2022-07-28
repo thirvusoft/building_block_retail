91a1b7754606b6ee46f12705b369e332064326ca
manikandaprabhu123 <manikanda.prabhu@thirvusoft.in>
2022-07-28 13:36:56 +0530

    Chore: Minor changes

--- building_block_retail/building_block_retail/utils/property_setter.py
+++ building_block_retail/building_block_retail/utils/property_setter.py
@@ -8,3 +8,7 @@ def create_property_setter():
     make_property_setter('Payment Entry', 'mode_of_payment', 'default', 'Cash', 'Text Editor')
     make_property_setter('Payment Entry', 'section_break_14', 'depends_on', 'eval:(doc.party && doc.paid_from && doc.paid_to)', 'Text Editor')
     make_property_setter('Sales Invoice', 'outstanding_amount', 'in_list_view', 1, 'Check')
+    make_property_setter('Purchase Receipt','apply_putaway_rule','hidden',1,'Check')
+    make_property_setter('Stock Entry','apply_putaway_rule','hidden',1,'Check')
+    make_property_setter('BOM','routing','hidden',1,'Check')
+    make_property_setter('Item','weight_per_unit','depends_on','eval:doc.item_group == "Pavers"','Text Editor')
