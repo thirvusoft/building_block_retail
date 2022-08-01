ea3ad24b250850b10c89f817099db9563c2097d5
root <root@ip-172-26-3-215.ap-south-1.compute.internal>
2022-08-01 11:00:15 +0000

    chores: ignore price list rate in PR when conv fact changed

--- building_block_retail/building_block_retail/custom/js/purchase_receipt.js
+++ building_block_retail/building_block_retail/custom/js/purchase_receipt.js
@@ -1,11 +1,13 @@
 frappe.ui.form.on("Purchase Receipt" ,{
-          onload:function(frm){
-                    frm.set_query('transporter', function(frm){
-                              return {
-                                  filters:{
-                                      'supplier_name': 'Own Transporter'
-                                  }
-                              }
-                          });
-          }
+    onload:function(frm){
+        frm.set_query('transporter', function(frm){
+                    return {
+                        filters:{
+                            'supplier_name': 'Own Transporter'
+                        }
+                    }
+                });
+        // To ignore price list while changing conversion factor
+        frappe.flags.dont_fetch_price_list_rate = true
+        }
 })
