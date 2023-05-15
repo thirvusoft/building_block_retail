9d19abaf6ed4767c2f5802dddd618e831f37b2ef
Shankarv19bcr <shankar@thirvusoft.in>
2023-05-15 12:06:58 +0530

    job worker report customize

--- building_block_retail/building_block_retail/report/thirvu_job_worker/thirvu_job_worker.py
+++ building_block_retail/building_block_retail/report/thirvu_job_worker/thirvu_job_worker.py
@@ -2,8 +2,10 @@
 # For license information, please see license.txt
 
 import frappe
-from frappe import _, errprint
+from frappe import _
+
 def execute(filters=None):
+    get_data(filters)
     from_date = filters.get("from_date")
     to_date = filters.get("to_date")
     employee = filters.get("employee")
@@ -68,7 +70,7 @@ def execute(filters=None):
         total[7] = sum(data[i][7]or 0 for i in range(start,len(data)))
         final_data.append(total)
     columns = get_columns()
-    return columns, final_data
+    return columns, get_data(filters)
 
 def get_columns():
     columns = [
@@ -78,11 +80,95 @@ def get_columns():
         _("Completed Sqft") + ":Data:150",
         _("Salary Balance") + ":Currency:150",
         _("Amount") + ":Currency:150",
-        _("Advance Amount") + ":Currency:150",
+        # _("Advance Amount") + ":Currency:150",
         _("Total Amount") + ":Currency:150",
         ]
-    
+    columns = [
+        {
+            'fieldname':'job_worker',
+            'label':'Job Worker',
+            'fieldtype':'Data',
+            'width':150
+        },
+        {
+            'fieldname':'site_name',
+            'label':'Site Name',
+            'fieldtype':'Link',
+            'options':'Project',
+            'width':300
+        },
+        {
+            'fieldname':'status',
+            'label':'Status',
+            'fieldtype':'Data',
+            'width':150
+        },
+        {
+            'fieldname':'sqft_allocated',
+            'label':'Completed Sqft',
+            'fieldtype':'Float',
+            'width':150
+        },
+      
+        {
+            'fieldname':'amount',
+            'label':'Amount',
+            'fieldtype':'Currency',
+            'width':150
+        },
+          {
+            'fieldname':'salary_balance',
+            'label':'Salary Balance',
+            'fieldtype':'Currency',
+            'width':150
+        },
+        # {
+        #     'fieldname':'',
+        #     'label':'Job Worker',
+        #     'fieldtype':'Link',
+        #     'options':'Employee'
+        # },
+
+    ]
     return columns
 
 
 # frappe.db.sql("""select sum(empadv.advance_amount - empadv.return_amount) from `tabEmployee Advance` as empadv where empadv.employee = '{0}' and docstatus = 1""".format(data[i+0][0]))
+
+
+def get_data(filters= {}):
+    final_data = []
+    condition = " 1 = 1 "
+    jw_filter = {}
+    if(filters.get('from_date') and filters.get('to_date')):
+        jw_filter['date'] = ['between', (filters.get('from_date'), filters.get('to_date'))]
+    elif(filters.get('from_date')):
+        jw_filter['date'] = [ ">=", filters.get('from_date')]
+    elif(filters.get('to_date')):
+        jw_filter['date'] = [ "<=", filters.get('to_date')]
+    
+    if(filters.get('employee')):
+        jw_filter['name1'] = filters.get('employee')
+    if(filters.get('site_name')):
+        jw_filter['parent'] = filters.get('site_name')
+
+    
+    data = frappe.db.get_all("Finalised Job Worker Details", filters=jw_filter, fields = ['name1 as employee', 'date', 'parent as site_name', 'sum(sqft_allocated) as sqft_allocated', 'sum(amount) as amount'], group_by = "name1, parent", order_by = "name1")
+    sqft, amt, sal_bal = 0,0,0
+    for i in range(len(data)):
+        data[i]['job_worker'] = frappe.db.get_value('Employee', data[i]['employee'], 'employee_name')
+        data[i]['status'] = frappe.db.get_value('Project', data[i]['site_name'], 'status')
+        data[i]['salary_balance'] = data[i]['amount'] - (frappe.db.sql(f"""
+        select sum(paid_amount) from `tabSite work Details` swd left outer join `tabSalary Slip` ss on ss.name = swd.parent where swd.site_work_name = "{data[i]['site_name']}" and ss.employee = "{data[i]['job_worker']}" 
+        """, as_list=1)[0][0] or 0)
+        final_data.append(data[i])
+        sqft += data[i]['sqft_allocated']
+        amt += data[i]['amount']
+        sal_bal += data[i]['salary_balance']
+        if(i<(len(data)-1)):
+            if(data[i]['employee'] != data[i+1]['employee']):
+                final_data.append({'status':'Total', 'sqft_allocated':sqft, 'amount':amt, 'salary_balance':sal_bal})
+                sqft, amt, sal_bal = 0,0,0
+    if(len(data)):
+        final_data.append({'status':'Total', 'sqft_allocated':sqft, 'amount':amt, 'salary_balance':sal_bal})
+    return final_data
