37ab93ed0e764f6fd109e1accb772eeeb7be6f38
manikandaprabhu123 <manikanda.prabhu@thirvusoft.in>
2023-05-17 16:17:31 +0530

    Feat: Update the item wise production Table

--- building_block_retail/building_block_retail/doctype/production_order/production_order.py
+++ building_block_retail/building_block_retail/doctype/production_order/production_order.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2023, Thirvusoft and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 import json
 from frappe.model.document import Document
 
@@ -10,13 +10,18 @@ class ProductionOrder(Document):
 		tot_qty = {}
 		for i in self.production_order_details:
 			if i.item_code not in tot_qty:
-				tot_qty[i.item_code] = i.qty_to_produced
+				tot_qty[i.item_code] = {
+					'qty_to_produce': i.qty_to_produced,
+					'color': i.color
+				}
 			else:
-				tot_qty[i.item_code] += i.qty_to_produced
+				tot_qty[i.item_code]['qty_to_produce'] += i.qty_to_produced
 		self.item_wise_production_qty = []
-		for m in tot_qty:
-			
+		for item_code, item_details in tot_qty.items():
+			qty_to_produce = item_details['qty_to_produce']
+			color = item_details['color']
 			self.append('item_wise_production_qty', dict(
-				item_code = m,
-				qty=tot_qty[m],
+				item_code = item_code,
+				qty=qty_to_produce,
+				color=color
 			))
