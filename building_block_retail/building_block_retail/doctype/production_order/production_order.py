454fb87370a70bf046aa4bb7f11d02c4a711424f
manikandaprabhu123 <manikanda.prabhu@thirvusoft.in>
2023-05-17 13:34:57 +0530

    Feat: Update the item wise production Table

--- building_block_retail/building_block_retail/doctype/production_order/production_order.py
+++ building_block_retail/building_block_retail/doctype/production_order/production_order.py
@@ -2,7 +2,22 @@
 # For license information, please see license.txt
 
 # import frappe
+import json
 from frappe.model.document import Document
 
 class ProductionOrder(Document):
-	pass
+	def validate(self):
+		tot_qty = {}
+		tot = []
+		for i in self.production_order_details:
+			if i.item_code not in tot_qty:
+				tot_qty[i.item_code] = i.qty_to_produced
+			else:
+				tot_qty[i.item_code] += i.qty_to_produced
+		self.item_wise_production_qty = []
+		for m in tot_qty:
+			
+			self.append('item_wise_production_qty', dict(
+				item_code = m,
+				qty=tot_qty[m],
+			))
