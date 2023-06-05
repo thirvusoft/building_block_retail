75fc858ab9a5904b1b312fe73ddb6e1a7f3e0a30
Nandhinidevi123 <nandhini.devi@thirvusoft.in>
2023-06-05 17:30:29 +0530

    change the flow for get items

--- building_block_retail/building_block_retail/custom/js/workstation.js
+++ building_block_retail/building_block_retail/custom/js/workstation.js
@@ -12,19 +12,61 @@ frappe.ui.form.on("Workstation", {
             }
         })
     },
-    get_items: async function (frm) {
+    get_items: async function (frm,cdt,cdn) {
+        let items = [];
+        (frm.doc.item_wise_production_capacity  || []).forEach(d=>{
+            items.push(d.item_code)
+        })
+        let temp_items = [];
+        (frm.doc.item_template  || []).forEach(d=>{
+            temp_items.push(d.template)
+        })
+    
         frappe.call({
-            method:"building_block_retail.building_block_retail.custom.py.item.item_list",
+            method:"building_block_retail.building_block_retail.custom.py.workstation.item_list",
+            args:{items:items,
+            template:temp_items},
             callback: function(r){
-                r.message.forEach((data)=>{
-                    var Workstation = frm.add_child("item_wise_production_capacity")
-                    Workstation.item_code = data["item_code"]
+              
+               if(r.message.length<3){
+                frm.set_df_property('item_template', 'hidden', 0);
                    
-                })
-                frm.refresh_field("item_wise_production_capacity")
+                r.message[0].forEach(value=>
+                 
+                    {
+                    var template_tab=frm.add_child("item_template")
+                    template_tab.template=value["item_code"]
+                    })
+                     r.message[1].forEach(value=>
+                 
+                        {
+                    var variant_tab=frm.add_child("item_wise_production_capacity")
+                    variant_tab.item_code=value["item_code"]
+                    variant_tab.variant_of=value["variant_of"]
+                   
+                        })
+                        
+                     frm.refresh_field("item_template")
+                     frm.refresh_field("item_wise_production_capacity")
+               }
+               else{
+                frm.set_df_property('item_template', 'hidden', 1);
+                r.message.forEach(value=>
+                    {
+                        var Workstation=frm.add_child("item_wise_production_capacity")
+                        Workstation.item_code=value["item_code"]
+                        Workstation.variant_of=value["variant_of"]
+                    })
+                    frm.refresh_field("item_wise_production_capacity")
+
+               }
+                
+               
             }
         })
+        frm.refresh_field("item_wise_production_capacity")
 	},
+    
     refresh:function(frm){
         frm.trigger('setup')
        
--- building_block_retail/building_block_retail/custom/py/workstation.py
+++ building_block_retail/building_block_retail/custom/py/workstation.py
@@ -4,9 +4,14 @@ def cal_per_hour(doc, actions):
     validate_for_duplicate_item(doc)
     total_hour= 0
     total_net_hour = 0
-    total_hour = doc.administrator_expense + total_hour
+    total_hour = doc.administrator_expense or 0 + total_hour
     total_net_hour = doc.hour_rate + total_net_hour
     doc.hour_rate = total_hour + total_net_hour
+    if doc.item_template and doc.item_wise_production_capacity:
+        for i in doc.item_template:
+            for j in doc.item_wise_production_capacity:
+                if (j.variant_of == i.template):
+                    j.production_capacity=i.qty
 
 def validate_for_duplicate_item(doc):
     items = {}
@@ -22,3 +27,41 @@ def validate_for_duplicate_item(doc):
             error_msg+=f"<p>Item: <b>{i}</b> entered {items[i][0]} times in Rows: <b>{', '.join(items[i][1])}</b></p>"
     if(error_msg):
         frappe.throw(error_msg)
+
+
+
+
+
+@frappe.whitelist()
+def item_list(items,template):
+    print(template)
+    item_group = []
+    parent_grps = []  
+    item_list=[]
+    item_list1=[]
+    final_list=[]
+    items=json.loads(items)
+    template=json.loads(template)
+  
+    settings_doc=frappe.get_doc("Manufacturing Settings","Manufacturing Settings")
+    settings_item_format=frappe.db.get_single_value("Manufacturing Settings","get_items")
+    for i in settings_doc.item_groups:
+        parent_grps.append(i.item_group)
+
+
+    for i in parent_grps:
+        item_group.extend(frappe.get_all('Item Group', filters={'parent_item_group':['in', i]}, pluck='name'))      
+        parent_grps.extend(frappe.get_all('Item Group', filters={'parent_item_group':['in', i], 'is_group':1}, pluck='name'))
+    item_group=item_group + parent_grps
+    if(settings_item_format == "Template"):
+        item_list=frappe.get_list("Item",filters={"item_group":["in",item_group],"has_variants":1,"name":["not in",template],"disabled":0},fields=["item_code"])
+        item_list1=frappe.get_list("Item",filters={"item_group":["in",item_group],"has_variants":0,"disabled":0,"name":["not in",items]},fields=["item_code","variant_of"])
+        final_list.append(item_list)
+        final_list.append(item_list1)
+        return final_list
+
+    else:
+        item_list=frappe.get_list("Item",filters={"item_group":["in",item_group],"has_variants":0,"disabled":0,"name":["not in",items]},fields=["item_code","variant_of"])
+        item_list.append({"item_format":"Variant"})
+        return item_list
+
