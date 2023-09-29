3a876f4349f3b5c2ec1ecc4c81b285cc4e883679
Shankarv19bcr <shankar@thirvusoft.in>
2023-09-29 17:08:29 +0530

    fixes

--- building_block_retail/building_block_retail/custom/js/supplier.js
+++ building_block_retail/building_block_retail/custom/js/supplier.js
@@ -1,5 +1,6 @@
 frappe.ui.form.on("Supplier",{
     refresh:function(frm){
+        console.log("SSSS")
         frappe.call({
             method: "building_block_retail.building_block_retail.custom.py.supplier.balance_purchase_receipt",
             args: {
@@ -8,7 +9,7 @@ frappe.ui.form.on("Supplier",{
             callback: function(r) { 
                 if(r.message[0]["balance"]){
                     var bal=Math.round(r.message[0]["balance"])
-                    let balance = `<p style="font-size:15px;font-weight:bold;">Balance Purchase Receipt Amount - ${bal}</p>`;
+                    let balance = `<p style="font-size:15px;font-weight:bold;">Balance Purchase Receipt Amount - ${fmt_money(bal)}</p>`;
                     frm.set_df_property('balance_purchase_receipt', 'options', balance); 
 
                 }
--- building_block_retail/building_block_retail/custom/py/supplier.py
+++ building_block_retail/building_block_retail/custom/py/supplier.py
@@ -6,6 +6,6 @@ def balance_purchase_receipt(supplier):
         """select sum(pr.rounded_total*(100-pr.per_billed)/100) as balance
         from `tabPurchase Receipt` pr
         where pr.supplier = "{0}" and
-        pr.docstatus !=2
+        pr.docstatus =1
         group by pr.supplier
         """.format(supplier),as_dict=1)
