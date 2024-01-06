36dc3818a82ee7132f9202fd30e33233fe71bbcd
Ponnusamy1-V <ponnusamy@thirvusoft.in>
2024-01-06 17:01:56 +0530

    Curing Chamber

---
+++ building_block_retail/building_block_retail/doctype/curing_chamber/curing_chamber.js
@@ -0,0 +1,282 @@
+// Copyright (c) 2024, Thirvusoft and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Curing Chamber', {
+	refresh: function (frm) {
+		frm.set_query("finished_warehouse", "items", function() {
+			return {
+				filters: {
+					is_group: 0
+				}
+			};
+		});
+		frm.set_query("scrap_warehouse", "items", function() {
+			return {
+				filters: {
+					is_group: 0
+				}
+			};
+		});
+		frm.set_query("employee", "items", function() {
+			return {
+				filters: {
+					status: "Active"
+				}
+			};
+		});
+
+
+		frm.fields_dict.items.grid.add_custom_button("Update Employee", async function onclick() {
+			let selected = frm.fields_dict.items.grid.get_selected_children();
+			if (!selected || !selected.length) {
+				frappe.show_alert({ "message": "Please select the rows", indicator: "red" })
+				return
+			}
+			let dialog = new frappe.ui.Dialog({
+				title: "Update Employee",
+				fields: [{
+					fieldname: "employee",
+					label: __("Employee"),
+					fieldtype: "Link",
+					options: "Employee",
+					reqd: 1,
+					get_query: function() {
+						return {
+							filters: {
+								status: "Active"
+							}
+						};
+					}
+				}],
+				primary_action_label: __('Update'),
+				primary_action: (data) => {
+					selected.forEach(r => {
+						frappe.model.set_value(r.doctype, r.name, 'employee', data.employee)
+					});
+					frappe.show_alert({message: 'Updated', indicator: 'green'});
+					dialog.hide()
+				}
+			});
+			dialog.show();
+		}).removeClass('btn-default btn-custom').addClass('btn-secondary');
+
+		frm.fields_dict.items.grid.add_custom_button("Update Finished Warehouse", async function onclick() {
+			let selected = frm.fields_dict.items.grid.get_selected_children();
+			if (!selected || !selected.length) {
+				frappe.show_alert({ "message": "Please select the rows", indicator: "red" })
+				return
+			}
+			let dialog = new frappe.ui.Dialog({
+				title: "Update Finished Warehouse",
+				fields: [{
+					fieldname: "warehouse",
+					label: __("Warehouse"),
+					fieldtype: "Link",
+					options: "Warehouse",
+					reqd: 1,
+					get_query: function() {
+						return {
+							filters: {
+								is_group: 0
+							}
+						};
+					}
+				}],
+				primary_action_label: __('Update'),
+				primary_action: (data) => {
+					selected.forEach(r => {
+						frappe.model.set_value(r.doctype, r.name, 'finished_warehouse', data.warehouse)
+					});
+					frappe.show_alert({message: 'Updated', indicator: 'green'});
+					dialog.hide()
+				}
+			});
+			dialog.show();
+		}).removeClass('btn-default btn-custom').addClass('btn-secondary');
+
+		frm.fields_dict.items.grid.add_custom_button("Update Scrap Warehouse", async function onclick() {
+			let selected = frm.fields_dict.items.grid.get_selected_children();
+			if (!selected || !selected.length) {
+				frappe.show_alert({ "message": "Please select the rows", indicator: "red" })
+				return
+			}
+			let dialog = new frappe.ui.Dialog({
+				title: "Update Scrap Warehouse",
+				fields: [{
+					fieldname: "warehouse",
+					label: __("Warehouse"),
+					fieldtype: "Link",
+					options: "Warehouse",
+					reqd: 1,
+					get_query: function() {
+						return {
+							filters: {
+								is_group: 0
+							}
+						};
+					}
+				}],
+				primary_action_label: __('Update'),
+				primary_action: (data) => {
+					selected.forEach(r => {
+						frappe.model.set_value(r.doctype, r.name, 'scrap_warehouse', data.warehouse)
+					});
+					frappe.show_alert({message: 'Updated', indicator: 'green'});
+					dialog.hide()
+				}
+			});
+			dialog.show();
+		}).removeClass('btn-default btn-custom').addClass('btn-secondary');
+	},
+	fetch_items: function (frm) {
+		if ((frm.doc.items || []).length > 0) {
+			frm.scroll_to_field("items");
+			frappe.confirm(
+				"The existing data in the items table will gets erased. Are you sure?",
+				() => {
+					frm.trigger("__fetch_items");
+				}
+			);
+		} else {
+			frm.trigger("__fetch_items");
+		}
+	},
+	__fetch_items: async function (frm) {
+		if (frm.is_new()) {
+			await frm.save()
+		}
+		await frappe.call({
+			method: "run_doc_method",
+			args: { 'docs': frm.doc, 'method': "fetch_items" },
+			freeze: true,
+			freeze_message: 'Fetching...',
+			callback: function (r) {
+				if (!r.exc) {
+					frm.refresh_fields();
+					frm.refresh()
+				}
+			}
+		});
+	},
+	complete_curing: async function (frm) {
+		frappe.confirm(
+			"Complete the curing process.",
+			async () => {
+				if (frm.is_new()) {
+					await frm.save()
+				}
+				await frappe.call({
+					method: "run_doc_method",
+					args: { 'docs': frm.doc, 'method': "complete_curing" },
+					freeze: true,
+					freeze_message: 'Please Wait...',
+					callback: function (r) {
+						if (!r.exc) {
+							frm.refresh_fields();
+							frm.refresh()
+						}
+					}
+				});
+			}
+		);
+	}
+});
+
+frappe.ui.form.on("Curing Chamber Items", {
+	to_bundle_qty: function (frm, cdt, cdn) {
+		let data = locals[cdt][cdn];
+		if ((data.to_bundle_qty || 0) > ((data.before_remaining_qty || 0) + (data.production_qty || 0) - (data.today_remaining_qty || 0))) {
+			frappe.show_alert({
+				message: "Incorrect value for <b>To bundle qty</b>.",
+				indicator: "red"
+			});
+		}
+		frm.trigger("__calc_damaged_qty", cdt, cdn);
+	},
+	today_remaining_qty: function (frm, cdt, cdn) {
+		let data = locals[cdt][cdn];
+		if ((data.today_remaining_qty || 0) > ((data.before_remaining_qty || 0) + (data.production_qty || 0) - (data.to_bundle_qty || 0))) {
+			frappe.show_alert({
+				message: "Incorrect value for <b>Today Remaining qty</b>",
+				indicator: "red"
+			});
+		}
+		frm.trigger("__calc_damaged_qty", cdt, cdn);
+	},
+	__calc_damaged_qty: function (frm, cdt, cdn) {
+		let data = locals[cdt][cdn];
+		let damage_qty = (data.before_remaining_qty || 0) + (data.production_qty || 0) - (data.to_bundle_qty || 0) - (data.today_remaining_qty || 0)
+		frappe.model.set_value(cdt, cdn, 'damaged_qty', damage_qty);
+	},
+	split_item: function (frm, cdt, cdn) {
+		let data = locals[cdt][cdn];
+		let dialog = new frappe.ui.Dialog({
+			title: "Split Qty",
+			fields: [
+				{
+					fieldname: "item",
+					label: "Item",
+					fieldtype: "Link",
+					options: "Item",
+					read_only: 1,
+					default: data.item
+				},
+				{
+					label: "Qty",
+					fieldtype: 'Section Break'
+				},
+				{
+					fieldname: "before_remaining_qty",
+					label: "Before Date Remaining Qty",
+					fieldtype: "Float",
+					read_only: 1,
+					default: data.before_remaining_qty
+				},
+				{
+					fieldname: "production_qty",
+					label: "Production Qty",
+					fieldtype: "Float",
+					read_only: 1,
+					default: data.production_qty
+				},
+				{
+					label: "Qty to Split",
+					fieldtype: "Column Break"
+				},
+				{
+					fieldname: "split_before_remaining_qty",
+					label: "Before Date Remaining Qty (To Split)",
+					fieldtype: "Float",
+				},
+				{
+					fieldname: "split_production_qty",
+					label: "Production Qty (To Split)",
+					fieldtype: "Float",
+				},
+			],
+			primary_action_label: 'Split',
+			primary_action: function (res) {
+				if ((res.split_before_remaining_qty > data.before_remaining_qty) || (res.split_production_qty > res.production_qty)) {
+					frappe.msgprint("Split qty must be less than actual qty");
+					return;
+				}
+				let row = frm.fields_dict.items.grid.add_new_row(data.idx + 1);
+				row.item = data.item
+				row.employee = data.employee
+				row.before_remaining_qty = res.split_before_remaining_qty
+				row.production_qty = res.split_production_qty
+				row.finished_warehouse = data.finished_warehouse
+				row.scrap_warehouse = data.scrap_warehouse
+				
+				frappe.model.set_value(data.doctype, data.name, 'before_remaining_qty', (data.before_remaining_qty || 0) - (res.split_before_remaining_qty || 0))
+				frappe.model.set_value(data.doctype, data.name, 'production_qty', (data.production_qty || 0) - (res.split_production_qty || 0))
+				if (data.to_bundle_qty > ((data.before_remaining_qty || 0) + (data.production_qty || 0))) {
+					frappe.model.set_value(data.doctype, data.name, 'to_bundle_qty', 0)
+				}
+				frm.fields_dict.items.grid.refresh();
+				dialog.hide();
+			}
+		});
+		dialog.show();
+	}
+});
---
+++ building_block_retail/building_block_retail/doctype/curing_chamber/curing_chamber.py
@@ -0,0 +1,228 @@
+# Copyright (c) 2024, Thirvusoft and contributors
+# For license information, please see license.txt
+
+from building_block_retail import uom_conversion
+import erpnext
+import frappe
+from frappe.model.document import Document
+from frappe.utils.data import get_link_to_form
+
+class CuringChamber(Document):
+	def validate(self):
+		for row in self.items:
+			if ((row.to_bundle_qty or 0) + (row.today_remaining_qty or 0)) > ((row.production_qty or 0) + (row.before_remaining_qty or 0)):
+				frappe.throw(msg=f"Incorrect values in row {row.idx}")
+			row.damaged_qty = ((row.production_qty or 0) + (row.before_remaining_qty or 0)) - ((row.to_bundle_qty or 0) + (row.today_remaining_qty or 0))
+
+	def validate_curing(self):
+		self.validate()
+		for row in self.items:
+			if not row.to_bundle_qty:
+				frappe.throw(f"Please enter <b>To Bundle Qty</b> in row {row.idx} or remove this row.")
+
+			if not row.not_bundled and not row.employee:
+				frappe.throw(f"Employee is required in row {row.idx}")
+			
+			if not row.finished_warehouse:
+				frappe.throw(f"Finished Warehouse is required in row {row.idx}")
+			
+			if row.damaged_qty and not row.scrap_warehouse:
+				frappe.throw(f"Scrap Warehouse is required in row {row.idx}")
+	
+	@frappe.whitelist()
+	def complete_curing(self):
+		if (cc:=frappe.get_all("Stock Entry", {"curing_chamber": self.name, 'docstatus': ["!=", 2]}, pluck='name')):
+			frappe.throw(f"""Curing process is already completed.
+				<ul>
+				{"".join([f"<li><a href='/app/stock-entry/{i}'>{i}</a></li>" for i in cc])}
+				</ul>""")
+
+		self.validate_curing()
+		
+		item_job_cards = {}
+
+		stock_entry = frappe.new_doc("Stock Entry")
+		se_items = []
+		for row in self.items:
+			bundle_salary_per_piece = frappe.db.get_value("Item", row.item, "bundling_cost")
+			if not bundle_salary_per_piece:
+				frappe.throw(f"""Missing Bundle cost in Item <b>{get_link_to_form("Item", row.item)}</b> """)
+			
+			if row.item not in item_job_cards:
+				before_date_items = frappe.db.get_all("Job Card", [
+					['docstatus', '=', 1],
+					['to_curing', '=', 1],
+					['curing_percent', '<', 100],
+					['production_item', '=', row.item],
+					['Job Card Time Log', 'from_time', '<', f"{self.date} 00:00:00"]
+				], ['name', 'for_quantity', 'curing_completed_qty', 'target_warehouse'])
+
+				on_date_items = frappe.db.get_all("Job Card", [
+					['docstatus', '=', 1],
+					['to_curing', '=', 1],
+					['curing_percent', '<', 100],
+					['production_item', '=', row.item],
+					['Job Card Time Log', 'from_time', 'between', [f"{self.date} 00:00:00", f"{self.date} 23:59:59"]]
+				], ['name', 'for_quantity', 'curing_completed_qty', 'target_warehouse'])
+
+				item_job_cards[row.item] = before_date_items + on_date_items
+
+			remaining_qty = row.to_bundle_qty
+			damaged_qty = row.damaged_qty
+			
+			job_cards = item_job_cards[row.item]
+
+			jc_idx = 0
+			while jc_idx < len(job_cards):
+				job_card = job_cards[jc_idx]
+				if not frappe.db.get_all("Stock Entry", {"ts_job_card": job_card.name, "docstatus": 1}):
+					jc_idx += 1
+					continue
+
+				if not remaining_qty and not damaged_qty:
+					break
+				
+				if 'qty' not in job_card:
+					job_card.qty = ((job_card.for_quantity or 0) - (job_card.curing_completed_qty or 0))
+				
+				if not job_card.qty:
+					jc_idx += 1
+					continue
+
+				if remaining_qty:
+					qty = job_card.qty
+					if job_card.qty > remaining_qty:
+						qty = remaining_qty
+						remaining_qty = 0
+					else:
+						remaining_qty -= job_card.qty
+
+					job_card.qty -= qty
+
+					if (job_card.target_warehouse == row.finished_warehouse):
+						frappe.throw(f"Source and Target Warehouse <b>({row.finished_warehouse})</b> cannot be same for row {row.idx}")
+
+					se_items.append({
+						"s_warehouse": job_card.target_warehouse,
+						"t_warehouse": row.finished_warehouse,
+						"item_code": row.item,
+						"qty": qty,
+						"conversion_factor": 1,
+						"from_job_card": job_card.name,
+						"bundling_employee": row.employee if not row.not_bundled else '',
+						"pcs_per_bundle": uom_conversion(item=row.item, from_uom='bundle', from_qty=1, to_uom='Nos'),
+						"no_of_bundle": row.to_bundle_qty / uom_conversion(item=row.item, from_uom='bundle', from_qty=1, to_uom='Nos'),
+						"bundling_cost": bundle_salary_per_piece * row.to_bundle_qty
+					})
+
+				if not remaining_qty and damaged_qty:
+					qty = job_card.qty
+					if job_card.qty > damaged_qty:
+						qty = damaged_qty
+						damaged_qty = 0
+					else:
+						damaged_qty -= job_card.qty
+
+					job_card.qty -= qty
+
+					if (job_card.target_warehouse == row.finished_warehouse):
+						frappe.throw(f"Source and Target Warehouse <b>({row.finished_warehouse})</b> cannot be same for row {row.idx}")
+					
+					se_items.append({
+						"s_warehouse": job_card.target_warehouse,
+						"t_warehouse": row.scrap_warehouse,
+						"item_code": row.item,
+						"qty": qty,
+						"conversion_factor": 1,
+						"from_job_card": job_card.name,
+						"pcs_per_bundle": uom_conversion(item=row.item, from_uom='bundle', from_qty=1, to_uom='Nos'),
+						"no_of_bundle": row.to_bundle_qty / uom_conversion(item=row.item, from_uom='bundle', from_qty=1, to_uom='Nos'),
+						"is_scrap_item": 1,
+						"is_process_loss": 1
+					})
+				
+				if job_card.qty == 0:
+					jc_idx += 1
+				
+				frappe.errprint(f"{row.item}  {jc_idx}  {len(se_items)}  {qty}   {job_card.qty}  {job_card.name}")
+
+			if (remaining_qty + damaged_qty) > 0:
+				frappe.throw(f"Insufficient Job Cards for Item <b>{row.item}</b>.<br>Expected Qty: {row.to_bundle_qty + row.damaged_qty}.<br>Shortage Qty: {remaining_qty + damaged_qty}")
+		
+		stock_entry.update({
+			'posting_date': self.posting_date,
+			'set_posting_time': 1,
+			'curing_completed': 1,
+			'curing_chamber': self.name,
+			'purpose': "Material Transfer",
+			'company': erpnext.get_default_company(),
+			"items": se_items,
+			"total_bundles": sum([(i.get("no_of_bundle") or 0) for i in se_items]),
+			"total_pcs": sum([(i.get("qty") or 0) for i in se_items]),
+			"total_salary": sum([(i.get("bundling_cost") or 0) for i in se_items]),
+		})
+
+		stock_entry.set_stock_entry_type()
+
+		stock_entry.save()
+
+		self.status = 'Completed'
+		self.save()
+		frappe.msgprint(f"Stock Entry Created Successfully <a href='/app/stock-entry/{stock_entry.name}'>{stock_entry.name}</a>")
+
+	@frappe.whitelist()
+	def fetch_items(self):
+		if (cc:=frappe.get_all("Stock Entry", {"curing_chamber": self.name, 'docstatus': ["!=", 2]}, pluck='name')):
+			frappe.throw(f"""Curing process is already completed.
+				<ul>
+				{"".join([f"<li><a href='/app/stock-entry/{i}'>{i}</a></li>" for i in cc])}
+				</ul>""")
+
+		if not self.date:
+			frappe.throw("Please enter the date.")
+
+		before_date_items = frappe.db.get_all("Job Card", [
+			['docstatus', '=', 1],
+			['to_curing', '=', 1],
+			['curing_percent', '<', 100],
+			['Job Card Time Log', 'from_time', '<', f"{self.date} 00:00:00"]
+		], ['name', 'production_item', 'for_quantity', 'curing_completed_qty', 'target_warehouse'])
+
+		on_date_items = frappe.db.get_all("Job Card", [
+			['docstatus', '=', 1],
+			['to_curing', '=', 1],
+			['curing_percent', '<', 100],
+			['Job Card Time Log', 'from_time', 'between', [f"{self.date} 00:00:00", f"{self.date} 23:59:59"]]
+		], ['name', 'production_item', 'for_quantity', 'curing_completed_qty', 'target_warehouse'])
+
+		items = {}
+		for item in before_date_items:
+			if not frappe.db.get_all("Stock Entry", {"ts_job_card": item.name, "docstatus": 1}):
+				continue
+			
+			if item.production_item not in items:
+				items[item.production_item] = {
+						"item": item.production_item,
+						"before_remaining_qty": 0,
+					}
+			
+			items[item.production_item]["before_remaining_qty"] = (items[item.production_item].get("before_remaining_qty") or 0) + ((item.get("for_quantity") or 0) - (item.get("curing_completed_qty") or 0))
+		
+		for item in on_date_items:
+			if not frappe.db.get_all("Stock Entry", {"ts_job_card": item.name, "docstatus": 1}):
+				continue
+
+			if item.production_item not in items:
+				items[item.production_item] = {
+						"item": item.production_item,
+						"production_qty": 0,
+					}
+			items[item.production_item]["production_qty"] = (items[item.production_item].get("production_qty") or 0) + ((item.get("for_quantity") or 0) - (item.get("curing_completed_qty") or 0))
+		
+		self.update({
+			"items": list(items.values())
+		})
+		self.save()
+		
+		frappe.msgprint(msg="Items updated.", indicator='green', alert=True)
+		return self
